37f4dc7c148fc0954fd39fa65b65b890ee3d14bb
Dawn Nguyen <lophocpzo@gmail.com>
2024-02-27 11:39:51 +0700

    Logic valdate login

--- data/users/userData.js
+++ data/users/userData.js
@@ -14,6 +14,7 @@ class UserDataManager {
     }
 
     static getUserByUsername(username) {
+        this.userData = this.getUserData();
         for (let i = 0; i < this.userData.length; i++) {
             if (username == this.userData[i].username) {
                 return this.userData[i];
--- pages/user/login/script.js
+++ pages/user/login/script.js
@@ -1,15 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const username = document.getElementById('email-field');
+    const password = document.getElementById('password-field');
+    const loginButton = document.getElementById('login-button');
+
+    username.addEventListener('input', toggleButtonState);
+    password.addEventListener('input', toggleButtonState);
+
+    function toggleButtonState() {
+        // Check if both inputs have non-blank values
+        if (username.value.trim() !== '' && password.value.trim() !== '') {
+            loginButton.disabled = false;
+        } else {
+            loginButton.disabled = true;
+        }
+    }
+}
+);
+
+
 function onClickLogin() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const username = document.getElementById('email-field').value;
+    const password = document.getElementById('password-field').value;
 
     if (validateInput(username, password)) {
-        let userData = UserDataManager.getUserData();
         let user = UserDataManager.getUserByUsername(username);
 
-        if(user === null) {
+        if (user === null) {
             alert("User not found!");
-        } else if(password != user.password) {
+        } else if (password != user.password) {
             alert("Incorrect password!");
+        } else {
+            alert("Login successful!");
         }
     }
 }
@@ -19,9 +40,15 @@ function validateInput(username, password) {
 }
 
 function validateUsername(username) {
-
+    const usernameRegex = /^[a-zA-Z]+$/;
+    const check = username.match(usernameRegex);
+    if (!check) alert("User not found!");
+    return check;
 }
 
 function validatePassword(password) {
-
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const check = password.match(passwordRegex);
+    if (!check) alert("Incorrect password!");
+    return check;
 }
