d9ca411b6b8cb8299929460fbf12cffac92cc622
annona17 <luna207sn@gmail.com>
2024-02-29 22:56:44 +0700

    Exams

--- page/admin/script.js
+++ page/admin/script.js
@@ -1,34 +1,8 @@
-const menuButtonExams = document.getElementById('menuButtonExams');
-const menuListExams = document.getElementById('menuListExams');
-
-const menuButtonStudents = document.getElementById('menuButtonStudents');
-const menuListStudents = document.getElementById('menuListStudents');
-
-const menuButtonStatistics = document.getElementById('menuButtonStatistics');
-const menuListStatistics = document.getElementById('menuListStatistics');
-
-menuButtonExams.addEventListener('click', function() {
-    if (menuListExams.style.display === 'none') {
-        menuListExams.style.display = 'block';
+function toggleSpecificTime(selectElement) {
+    var specificTimeInput = document.getElementById("specificTimeInput");
+    if (selectElement.value === "specificTime") {
+        specificTimeModal.show();
     } else {
-        menuListExams.style.display = 'none';
+        specificTimeInput.style.display = "none";
     }
-});
-
-menuButtonStudents.addEventListener('click', function() {
-    if (menuListStudents.style.display === 'none') {
-        menuListStudents.style.display = 'block';
-    } else {
-        menuListStudents.style.display = 'none';
-    }
-});
-
-menuButtonStatistics.addEventListener('click', function() {
-    if (menuListStatistics.style.display === 'none') {
-        menuListStatistics.style.display = 'block';
-    } else {
-        menuListStatistics.style.display = 'none';
-    }
-});
-
-
+}
