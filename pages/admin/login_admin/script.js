e2bb1a726064b3355957b6f82ce18ea8716557db
Cao Việt Đức <112269340+alexcao194@users.noreply.github.com>
2024-03-04 17:10:55 +0700

    add go to admin

--- pages/admin/login_admin/script.js
+++ pages/admin/login_admin/script.js
@@ -1,21 +1,12 @@
-function toggleSpecificTime(selectElement) {
-    var specificTimeInput = document.getElementById("specificTimeInput");
-    if (selectElement.value === "specificTime") {
-        specificTimeModal.show();
-    } else {
-        specificTimeInput.style.display = "none";
-    }
-}
-
-var email_field = document.getElementById("email-field");
-var password_field = document.getElementById("password-field");
 var login_button = document.getElementById("login-button");
 
 login_button.addEventListener("click", function () {
+    var email_field = document.getElementById("email-field");
+    var password_field = document.getElementById("password-field");
     var email = email_field.value;
     var password = password_field.value;
-    if (email == "alexcao194@gmail.com" && password == "admin") {
-        window.location.href = "../dashboard/index.html";
+    if (email === "alexcao194@gmail.com" && password === "admin") {
+        window.location.replace("../../admin/dashboard/index.html");
     } else {
         alert("Invalid credentials");
     }
